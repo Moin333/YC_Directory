61bbdc2a313528dbcb7b83d695411215015aa2d2
Moin333 <ansarimoin7861@gmail.com>
2025-07-06 10:12:01 +0530

    startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {
   searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {_id: 1, name: "Moin"},
+      _id: 1,
+      description: "This is a description",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi3xunjYYcs9Ckm2RnwXzpZSmjVU2mmPOm1w&s",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -18,6 +32,22 @@ export default async function Home({searchParams}: {
         </p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+          <p className="text-30-semibold">
+            {query ? `Search results for "${query}"` : 'All Startups'}
+          </p>
+
+          <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post}/>
+              ))
+            ) : (
+              <p className="no-results">No startups found</p>
+            )}
+          </ul>
       </section>
     </>
   );
